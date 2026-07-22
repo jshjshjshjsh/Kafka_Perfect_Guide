d7648837897b6b21491eafb7c347b74cee5309c9
jshjshjshjsh <cj456456@gmail.com>
2026-07-22 22:40:40 +0900

    feat: file 감지해서 전송

---
+++ practice/src/main/java/com/pratice/kafka/event/EventHandler.java
@@ -0,0 +1,7 @@
+package com.pratice.kafka.event;
+
+import java.util.concurrent.ExecutionException;
+
+public interface EventHandler {
+    void onMessage(MessageEvent messageEvent) throws InterruptedException, ExecutionException;
+}
